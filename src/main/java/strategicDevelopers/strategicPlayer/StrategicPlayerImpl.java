de99f316a82964867cbd53c0cbec832c3ad5f972
tgolnoft <tgolnoft@gmail.com>
2018-10-12 15:04:20 -0600

    changed pom file and project structure to fix build issues
    added a shell main class to aid with testing
    
    began implementing getrevealpattern and relevant tests

--- src/main/java/strategicDevelopers/strategicPlayer/StrategicPlayerImpl.java
+++ src/main/java/strategicDevelopers/strategicPlayer/StrategicPlayerImpl.java
@@ -6,6 +6,21 @@ public class StrategicPlayerImpl implements StrategicPlayer {
    private int revealsPerSpin;
    private int maxNumSpins;
 
+   /**
+    * Default constructor, implements a 4 coin 2 reveal game with
+    * no max spins (represented by a maxNumSpins < 0)
+    */
+   public void StrategicPlayerImpl(){
+      beginGame(4, 2, -1);
+   }
+    
+   /**
+    * Constructor, with variable max spins
+    */
+   public void StrategicPlayerImpl(int maxNumSpins){
+      beginGame(4, 2, maxNumSpins);
+   }
+  
    @Override
    public void beginGame(int coinsPerWheel, int revealsPerSpin, int maxNumSpins) {
       this.coinsPerWheel = coinsPerWheel;
@@ -15,7 +30,10 @@ public class StrategicPlayerImpl implements StrategicPlayer {
 
    @Override
    public CharSequence getSlotsToReveal() {
-      return null;
+     if(currentSpinCount%2 == 0)
+       return "?-?-";
+     else
+       return "??--";
    }
 
    @Override
---
+++ src/test/java/strategicDevelopers/strategicPlayer/revealTests.java
@@ -0,0 +1,13 @@
+package strategicDevelopers.strategicPlayer;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class revealTests {
+  final String reveal0 = "?-?-";
+  final String reveal1 = "??--";
+  @Test
+  public void revealTest(){
+    StrategicPlayerImpl stratplay = new StrategicPlayerImpl();
+    
+  }
+}
