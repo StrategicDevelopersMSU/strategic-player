b6719eb7a4b91c20c78958b38da30cb58e81fcd9
Doug McLaughlin <dmclaughlin@homeadvisor.com>
2018-10-14 09:51:00 -0600

    strategy for new coin states

--- strategic-player/src/main/strategicDevelopers/strategicPlayer/TwoCoinFourSlotPlayer.java
+++ strategic-player/src/main/strategicDevelopers/strategicPlayer/TwoCoinFourSlotPlayer.java
@@ -1,6 +1,12 @@
 package strategicDevelopers.strategicPlayer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoCoinFourSlotPlayer implements StrategicPlayer {
+   private static final char HEADS_COIN_STATE = 'H';
+   private static final char TAILS_COIN_STATE = 'T';
+
    private int currentSpinCount = 0;
    private int coinsPerWheel;
    private int revealsPerSpin;
@@ -16,13 +22,85 @@ public class TwoCoinFourSlotPlayer implements StrategicPlayer {
 
    @Override
    public CharSequence getSlotsToReveal() {
-
       return null;
    }
 
    @Override
    public CharSequence getNewCoinStates(CharSequence revealedPattern) {
-      return null;
+      Map<String, Integer> numberOfCoinOccurences = getNumberOfCoinOccurrencesInPattern(revealedPattern);
+
+      setDesiredEndCoinState(numberOfCoinOccurences);
+
+      CharSequence strategicCoinPattern = replaceQuestionMarkWithDesiredCoinState(revealedPattern, desiredEndCoinState);
+
+      return strategicCoinPattern;
+   }
+
+   private CharSequence replaceQuestionMarkWithDesiredCoinState(CharSequence revealedPattern, String desiredEndCoinState)
+   {
+      StringBuilder stringBuilder = new StringBuilder();
+
+      for(int i = 0; i < revealedPattern.length(); i++)
+      {
+         char currentChar = revealedPattern.charAt(i);
+         if(currentChar == '?')
+         {
+            stringBuilder.append(desiredEndCoinState);
+            stringBuilder.append(" ");
+         }
+         else
+         {
+            stringBuilder.append(currentChar);
+            stringBuilder.append(" ");
+         }
+      }
+
+      return stringBuilder.toString();
+   }
+
+   private Map<String,Integer> getNumberOfCoinOccurrencesInPattern(CharSequence revealedPattern) {
+      int numOfHeads = 0;
+      int numOfTails = 0;
+      Map<String, Integer> coinOccurences = new HashMap<String, Integer>();
+
+      for(int i =0; i < revealedPattern.length(); i++)
+      {
+         char c = revealedPattern.charAt(i);
+         if(c == HEADS_COIN_STATE)
+         {
+            numOfHeads++;
+         }
+         if(c == TAILS_COIN_STATE)
+         {
+            numOfTails++;
+         }
+      }
+
+      coinOccurences.put("numOfHeads", numOfHeads);
+      coinOccurences.put("numOfTails", numOfTails);
+
+      return coinOccurences;
+   }
+
+   //If we get 2 of either on the first reveal and the game isnt over, we flip them both
+   //because at worst 3/4 coins are now that state, at best 4/4 are now in that state and
+   //the game is over.
+   private void setDesiredEndCoinState(Map<String, Integer> coinOccurrences)
+   {
+      int numOfHeadsInPattern = coinOccurrences.get("numOfHeads");
+      int numOfTailsInPattern = coinOccurrences.get("numOfTails");
+
+      if(currentSpinCount == 1)
+      {
+         if(numOfHeadsInPattern == 2)
+         {
+            desiredEndCoinState = "T";
+         }
+         if(numOfTailsInPattern == 2)
+         {
+            desiredEndCoinState = "H";
+         }
+      }
    }
 
    public int getCoinsPerWheel() {
